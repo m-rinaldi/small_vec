1280270fcac4a2ac01648466e6cf5278a135da81
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-08-15 12:24:33 +0200

    new() implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+use std::mem::MaybeUninit;
+
+pub enum SmallBuf<T, const N: usize> {
+    Local([MaybeUninit<T>; N], usize),
+    Remote(Vec<T>),
+}
+
+impl<T, const N: usize> SmallBuf<T, N> {
+    pub fn new() -> Self {
+        let uninit_arr:[MaybeUninit<T>; N] = unsafe {
+            MaybeUninit::uninit().assume_init()
+        };
+        // TODO use uninit_array() instead
+        Self::Local(uninit_arr, 0)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::SmallBuf;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_new() {
+        SmallBuf::<u8, 32>::new();
     }
 }

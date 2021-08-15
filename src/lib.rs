697a075af0ad9d52a91949b2bfe4d2e68e5f2519
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-08-15 12:35:58 +0200

    len() implemented

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,24 @@ impl<T, const N: usize> SmallBuf<T, N> {
         // TODO use uninit_array() instead
         Self::Local(uninit_arr, 0)
     }
+
+    pub fn len(&self) -> usize {
+        use SmallBuf::*;
+        match self {
+            Local(_,len) => *len,
+            Remote(vec) => vec.len(),
+        }
+    }
+
+    /* TODO
+    pub fn push(&mut self, val: T) {
+
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+
+    }
+    */
 }
 
 #[cfg(test)]
@@ -22,5 +40,11 @@ mod tests {
     #[test]
     fn test_new() {
         SmallBuf::<u8, 32>::new();
+    }
+
+    #[test]
+    fn test_zero_len() {
+        let buf = SmallBuf::<u8, 32>::new();
+        assert_eq!(buf.len(), 0);
     }
 }

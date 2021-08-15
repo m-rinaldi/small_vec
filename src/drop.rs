dc3f0cdf877b3d932c7d3ca61e2b62d11b70cb8d
J. Rinaldi <2664441+m-rinaldi@users.noreply.github.com>
2021-08-15 18:15:44 +0200

    Feature/drop (#3)
    * Drop implemented
    
    * unit tests for Drop impl

---
+++ src/drop.rs
@@ -0,0 +1,84 @@
+use crate::SmallBuf;
+
+impl<T, const N: usize> Drop for SmallBuf<T, N> {
+    fn drop(&mut self) {
+        match self {
+            Self::Local(_, _) => {
+                while let Some(val) = self.pop() {
+                    // superfluous
+                    std::mem::drop(val);
+                }
+            }
+            Self::Remote(_) => (),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    struct CounterGuard(*mut u8);
+
+    impl<'a> CounterGuard {
+        pub fn new(cnt: &'a mut u8) -> CounterGuard {
+            *cnt += 1;
+            CounterGuard(cnt as *mut u8)
+        }
+    }
+
+    impl<'a> Drop for CounterGuard {
+        fn drop(&mut self) {
+            unsafe {
+                *self.0 -= 1;
+            }
+        }
+    }
+
+    use crate::SmallBuf;
+
+    #[test]
+    fn test_drop_locally() {
+        let mut cnt = 0u8;
+        let mut buf = SmallBuf::<_, 3>::new();
+
+        assert_eq!(cnt, 0);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 1);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 2);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 3);
+
+        std::mem::drop(buf);
+        assert_eq!(cnt, 0);
+    }
+
+    #[test]
+    fn test_drop_remotely() {
+        let mut cnt = 0u8;
+        let mut buf = SmallBuf::<_, 3>::new();
+
+        assert_eq!(cnt, 0);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 1);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 2);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 3);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert!(buf.is_remote());
+        assert_eq!(cnt, 4);
+
+        buf.push(CounterGuard::new(&mut cnt));
+        assert_eq!(cnt, 5);
+
+        std::mem::drop(buf);
+        assert_eq!(cnt, 0);
+    }
+}

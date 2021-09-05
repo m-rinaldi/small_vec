097ba9424f7ca4e0ac2f05d1b900a9ad7e54b776
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-09-05 21:19:29 +0200

    test for Index implemented

--- src/index.rs
+++ src/index.rs
@@ -11,3 +11,39 @@ impl<T, const N: usize> Index<usize> for SmallVec<T, N> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_index() {
+        let mut vec = SmallVec::<_, 4>::new();
+        vec.push(0);
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+
+        assert!(vec.is_local());
+        assert_eq!(vec[0], 0);
+        assert_eq!(vec[1], 1);
+        assert_eq!(vec[2], 2);
+        assert_eq!(vec[3], 3);
+
+        vec.push(4);
+        vec.push(5);
+        vec.push(6);
+        assert!(vec.is_remote());
+
+        assert_eq!(vec[0], 0);
+        assert_eq!(vec[1], 1);
+        assert_eq!(vec[2], 2);
+        assert_eq!(vec[3], 3);
+        assert_eq!(vec[4], 4);
+        assert_eq!(vec[5], 5);
+        assert_eq!(vec[6], 6);
+
+    }
+
+    // TODO test out of bounds
+}
